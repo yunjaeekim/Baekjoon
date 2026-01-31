b573678064accb67bf53c9ba2ff6a5cf4dc12ccd
Kim yun jae <133327199+yunjaeekim@users.noreply.github.com>
2026-02-01 02:58:45 +0900

    Title: 흰검 칠하기, Time: 92ms, Memory: 22MB, Status: Passed - Codetree

--- 260201/흰검 칠하기/painting-white-black.py
+++ 260201/흰검 칠하기/painting-white-black.py
@@ -7,34 +7,34 @@ for num, direction in commands:
     dir.append(direction)
 
 # Please write your code here.
-ans = [0]*2*n*max(x)
+ans = ['']*2*n*max(x)
 crr = n*max(x)
 w_cnt, b_cnt, g_cnt = 0,0,0
 for dst, drt in zip(x, dir):
     if drt == 'L':
-        ans[crr-dst+1:crr+1] = [i+1 for i in ans[crr-dst+1:crr+1]]
+        ans[crr-dst+1:crr+1] = [i+'L' for i in ans[crr-dst+1:crr+1]]
         crr -= dst-1
     else:
-        ans[crr:crr+dst] = [i+10000 for i in ans[crr:crr+dst]]
+        ans[crr:crr+dst] = [i+'R' for i in ans[crr:crr+dst]]
         crr += dst-1
 
-for idx, val in enumerate(ans):
-    black = val // 10000
-    white = val % 10000
-    if val == 0:
+for word in ans:
+    if word == '':
         continue
-    elif (black >= 2) & (white >= 2):
+    R_cnt = 0
+    L_cnt = 0
+    for sp in word:
+        if sp == 'L':
+            L_cnt += 1
+        else:
+            R_cnt += 1
+    if (R_cnt >= 2) & (L_cnt >= 2):
         g_cnt += 1
-    elif idx >= crr:
-        if black > white:
-            b_cnt += 1
-        else:
-            w_cnt += 1
     else:
-        if white > black:
-            w_cnt += 1
-        else:
+        if word[-1] == 'R':
             b_cnt += 1
+        else:
+            w_cnt += 1
 
 print(w_cnt, b_cnt, g_cnt)
     
