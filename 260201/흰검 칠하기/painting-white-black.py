6938294bb2054d5f2e31b27d1ef0a66fcafb6900
Kim yun jae <133327199+yunjaeekim@users.noreply.github.com>
2026-02-01 02:30:55 +0900

    Title: 흰검 칠하기, Time: 66ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 260201/흰검 칠하기/painting-white-black.py
+++ 260201/흰검 칠하기/painting-white-black.py
@@ -7,53 +7,36 @@ for num, direction in commands:
     dir.append(direction)
 
 # Please write your code here.
-ans = []
-crr, past = 0, 'D'
+ans = [0]*2*n*max(x)
+crr = n*max(x)
+w_cnt, b_cnt, g_cnt = 0,0,0
 for dst, drt in zip(x, dir):
     if drt == 'L':
-        if drt == past:
-            past = 'L'
-            for i in range(dst):
-                ans.append(crr-i)
-            crr -= dst-1
-        else:
-            past = 'L'
-            for i in range(dst):
-                ans.append(crr-i)
-            crr -= dst-1
-    
+        ans[crr-dst+1:crr+1] = [i+1 for i in ans[crr-dst+1:crr+1]]
+        crr -= dst-1
     else:
-        if drt == past:
-            past = 'R'
-            for i in range(dst):
-                ans.append(crr+i)
-            crr += dst-1
-        else:
-            past = 'R'
-            for i in range(dst):
-                ans.append(crr+i)
-            crr += dst-1
+        ans[crr:crr+dst] = [i+1000 for i in ans[crr:crr+dst]]
+        crr += dst-1
 
-cnt = {}
-for x in ans:
-    cnt[x] = cnt.get(x, 0) + 1
-
-w_cnt, b_cnt, g_cnt = 0, 0, 0
-
-for x, c in cnt.items():
-    if c >= 4:
+for idx, val in enumerate(ans):
+    black = val // 1000
+    white = val % 1000
+    if val == 0:
+        continue
+    elif (black >= 2) & (white >= 2):
         g_cnt += 1
-    
-    else:
-        if x < 0:
-            if c % 2 == 0:
-                b_cnt += 1
-            else:
-                w_cnt += 1
+    elif idx >= crr:
+        if black > white:
+            b_cnt += 1
         else:
-            if c % 2 == 0:
-                w_cnt += 1
-            else:
-                b_cnt += 1
+            w_cnt += 1
+    else:
+        if white > black:
+            w_cnt += 1
+        else:
+            b_cnt += 1
 
-print(w_cnt,b_cnt,g_cnt)
+print(w_cnt, b_cnt, g_cnt)
+    
+
+        
