86f1a436d214d296896f826c79da94fcd3d6d47b
Kim yun jae <133327199+yunjaeekim@users.noreply.github.com>
2026-02-16 19:55:12 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260216/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -0,0 +1,36 @@
+n, m = map(int, input().split())
+arr = [[0] * m for _ in range(n)]
+
+# Please write your code here.
+def keep_move(arr,m,n,r,c):
+    if (1<=r<=n) & (1<=c<=m):
+        if arr[r-1][c-1] == 0:
+            return True
+    return False
+
+def turn(arr,r,c):
+    return (arr[r][c] != 0)
+
+step = {
+    0:(0,1),
+    1:(1,0),
+    2:(0,-1),
+    3:(-1,0)
+}
+r,c,dir = 1,1,0
+arr[0][0] = "1"
+
+for i in range(2,n*m+1):
+    nr,nc = step[dir]
+    if keep_move(arr,m,n,r+nr,c+nc):
+        r += nr
+        c += nc
+        arr[r-1][c-1] = str(i)
+    else:
+        dir = (dir+1) % 4
+        nr,nc = step[dir]
+        r += nr
+        c += nc
+        arr[r-1][c-1] = str(i)
+
+print("\n".join([" ".join(row) for row in arr]))
