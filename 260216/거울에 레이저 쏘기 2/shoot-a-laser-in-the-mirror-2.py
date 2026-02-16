bd0d910666bcec8fce4a81ed06cf4f94bb3eb978
Kim yun jae <133327199+yunjaeekim@users.noreply.github.com>
2026-02-16 21:19:55 +0900

    Title: 거울에 레이저 쏘기 2, Time: 132ms, Memory: 47MB, Status: Wrong Answer - Codetree

--- 260216/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 260216/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -1,51 +1,37 @@
 n = int(input())
-grid = [list(input()) for _ in range(n)]
+arr = [list(input()) for _ in range(n)]
 k = int(input())
 
 # Please write your code here.
-if 1<=k<=n:
-    dir = 1
+if 1<=k<=n:         #아래
     r,c = 1,k%n
-elif n+1<=k<=2*n:
-    dir = 0
+    nx,ny = 1,0
+elif n+1<=k<=2*n:   #왼쪽
     r,c = k%n,n
-elif 2*n+1<=k<=3*n:
-    dir = 3
+    nx,ny = 0,-1
+elif 2*n+1<=k<=3*n: #위
     r,c = n, k%n
-else:
-    dir = 2
+    nx,ny = -1,0
+else:               #오른쪽
     r,c = k%n, 1
-
-dir_to_point = {
-    0 : (0,-1),
-    1 : (1,0),
-    2 : (0,1),
-    3 : (-1,0)
-}
-
-def change_dir(dir, shape):
-    if shape == '/':
-        if 0<=dir<=1:
-            return 1-dir
-        else:
-            return 5-dir
-    else:
-        if (dir==0) & (dir==3):
-            return 3-dir
-        else:
-            return 3-dir
+    nx,ny = 0,1
 
 def in_range(n,r,c):
     return (1<=r<=n) & (1<=c<=n)
 
+def move(r,c,nx,ny):
+    if arr[r-1][c-1] == '/':
+        nx,ny = -ny, -nx
+    else:
+        nx,ny = ny, nx
+    return r+nx, c+ny, nx, ny
+
 ans = 0
-while (in_range(n,r,c)):
-    shape = grid[r-1][c-1]
-    dir = change_dir(dir,shape)
-    nx,ny = dir_to_point[dir]
-    r += nx
-    c += ny
+while in_range(n,r,c):
+    r, c, nx, ny = move(r,c,nx,ny)
     ans += 1
 print(ans)
+
+
 
 
