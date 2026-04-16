5d8b70e57cc4f0ccc874f64a3dda16c492f73550
Kim yun jae <133327199+yunjaeekim@users.noreply.github.com>
2026-04-16 20:32:46 +0900

    Title: Carry 피하기 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260416/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,43 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+N = len(str(max(arr)))
+
+# Please write your code here.
+def make_lst(num, N):
+    lst, K = [], 10**(N-1)
+    while K >= 1:
+        lst.append(int(num//K))
+        num = num % K
+        K /= 10
+    return lst
+
+def make_num(lst,N):
+    idx,num = 1,0
+    for val in lst:
+        num += val*10**(N-idx)
+        idx+=1
+    return num
+
+def is_carry(lst):
+    for val in lst:
+        if val > 9:
+            return True
+    return False
+
+lst = [make_lst(num,N) for num in arr]
+ans = [0,0,0,0]
+
+for i in range(N-2):
+    first = lst[i]
+    for j in range(i+1,N-1):
+        second = lst[j]
+        fir_sec = [i+j for i,j in zip(first,second)]
+        if not is_carry(fir_sec):
+            for k in range(j+1,N):
+                third = lst[k]
+                cand = [i+j for i,j in zip(fir_sec,third)]
+                if not is_carry(cand) and make_num(cand,N)>make_num(ans,N):
+                    ans = cand
+
+print(make_num(ans,N))
+
